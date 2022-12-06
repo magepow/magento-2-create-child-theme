cbfeeafba0e26c09f801879af2407ae24ba5d14a
ngoducnhuiit <ngoducnhuiit@gmail.com>
2022-12-06 14:23:39 +0700

    Update php 8.x

--- Controller/Adminhtml/Action.php
+++ Controller/Adminhtml/Action.php
@@ -112,7 +112,7 @@ abstract class Action extends \Magento\Backend\App\Action
     {
         $namespace = (new \ReflectionObject($this))->getNamespaceName();
         $string = strtolower(str_replace(__NAMESPACE__ . '\\','', $namespace));
-        $action =  explode('\\', $string);
+        $action =  explode('\\',(string) $string);
         $action =  array_shift($action);
         return $this->_authorization->isAllowed("Magepow_Theme::theme_$action");
     }
